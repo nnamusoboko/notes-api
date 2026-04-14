03d58867537c49be6c132ea78e34a1c2ea8c11f7
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-14 16:19:16 +0300

    refactor: routes from main app

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,10 @@
 import express from "express";
+import routes from './routes/routes.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/health', (_req, res) => {
-    res.status(200).json({"message": "Server running"});
-});
-
-app.post('/api/notes', (_req, res) => {
-    res.status(201).json({"message": "soon creating new notes"});
-});
-app.get('/api/notes', (_req, res) => {
-    res.status(200).json({"message": "soon returning created notes"});
-});
-app.get('/api/notes/:id', (_req, res) => {
-    res.status(200).json({"message": "soon returning contents of a note"});
-});
-app.patch('/api/notes/:id', (_req, res) => {
-    res.status(200).json({"message": "soon updating note"});
-});
-app.delete('/api/note/:id', (_req, res) => {
-    res.status(204).json({"message": "soon deleting created note"});
-});
+app.use('/api', routes);
 
 export default app;
