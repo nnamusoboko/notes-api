2472562a4bdb153ac4b09939b7fbf33e7f1d65be
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-15 00:25:01 +0300

    feat: add remove note function in service

--- src/services/notes-service.ts
+++ src/services/notes-service.ts
@@ -2,9 +2,7 @@ import type { CreateNoteRequest, CreateNoteResponse, Note, UpdateNoteRequest } f
 import NotesRepo from '../repositories/notes-repo.js';
 
 class NotesService {
-    create = async (note: CreateNoteRequest): Promise<CreateNoteResponse> => {
-        console.log(`[SENT NOTE]: ${note}`);
-        
+    create = async (note: CreateNoteRequest): Promise<CreateNoteResponse> => {        
         return NotesRepo.saveNote(note);
     }
 
@@ -18,6 +16,16 @@ class NotesService {
 
     updateNote = async (noteId: string, noteInfo: UpdateNoteRequest): Promise<Note | undefined> => {
         return NotesRepo.updateNote(noteId, noteInfo);
+    }
+
+    remove = async (noteId: string): Promise<void> => {
+        const result: boolean = await NotesRepo.removeNote(noteId);
+
+        if (result) {
+            return console.log(`Note removed`);
+        }
+
+        throw new Error("NOT_FOUND");
     }
 }
 
