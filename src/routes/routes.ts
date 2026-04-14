3c28f5dc6f6c964100024500184fe44de01bfb8d
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-15 00:57:08 +0300

    refactor: move route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,14 +3,14 @@ import Handler from '../controllers/notes-controller.js';
 
 const router = express.Router();
 
-router.get('/health', (_req, res) => {
-    res.status(200).json({"message": "Server running"});
-});
-
 router.post('/notes', Handler.createNote);
 router.get('/notes', Handler.getAllNotes);
 router.get('/notes/:id', Handler.getSingleNote);
 router.patch('/notes/:id', Handler.updateSingleNote);
 router.delete('/notes/:id', Handler.deleteNote);
+
+router.get('/health', (_req, res) => {
+    res.status(200).json({"message": "Server running"});
+});
 
 export default router;
