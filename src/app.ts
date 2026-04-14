b56a95f51f594b4f42c8e778c309f7c814eba290
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-14 16:00:07 +0300

    feat: apply middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/health', (_req, res) => {
     res.status(200).json({"message": "Server running"});
 });
