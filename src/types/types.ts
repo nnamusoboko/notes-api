48fcdf2b38c70d024bbb4a813484a8169ce5b698
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-15 17:55:19 +0300

    refactor: make fields compulsary coz every note needs them

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,8 +2,8 @@ export interface Note {
     id: string
     title: string,
     contents: string,
-    createdAt?: Date,
-    updatedAt?: Date
+    createdAt: Date,
+    updatedAt: Date
 }
 
 export type CreateNoteRequest = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>
