dde54cd447cdd7b83a7a829fc02dd2177813464d
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-18 16:26:14 +0300

    feat: create paginatedNotes and response contracts for user required data

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,5 +6,19 @@ export interface Note {
     updatedAt: Date
 }
 
+export interface NotesMetaData {
+    totalCount: number,
+    totalPages: number,
+    currentPage: number,
+    limit: number,
+    hasNext: boolean,
+    hasPrev: boolean
+}
+
+export interface PaginatedResponse {
+    notes: Note[],
+    meta: NotesMetaData
+}
+
 export type CreateNoteRequest = Omit<Note, 'id' | 'createdAt' | 'updatedAt'>
 export type UpdateNoteRequest = Partial<Pick<Note, 'title' | 'contents'>>
