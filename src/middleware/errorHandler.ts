4401a296509479c04ee65ec1bb8880a1fc85a725
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-20 12:46:27 +0300

    feat: implement gobla error handler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from 'express';
+import { AppError } from "../utils/error.js";
+
+export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
+    console.error('[ERROR STACK]: ', err); // internal
+
+    const isCustomError = err instanceof AppError;
+    const statusCode = isCustomError ? err.statusCode : 500;
+    const message = isCustomError ? err.message : 'Internal Server Error';
+
+    res.status(statusCode).json(message);
+}
