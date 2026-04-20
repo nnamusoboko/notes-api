2db09120a52f1cca5fda5a3f283d6b58c4b1902e
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-20 18:20:15 +0300

    fix: prevent division by zero, throw error when limit is 0

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,10 +1,16 @@
 import type { NotesMetaData } from "../types/types.js";
+import { AppError } from "./error.js";
+import { HTTP_STATUS } from "./constants.js";
 
 export function getMetaData(page: number, limit: number, notesCount: number): NotesMetaData  {
    let totalPages: number;  
    let hasPrev: boolean, hasNext: boolean;
 
    const totalCount: number = notesCount;
+
+   if (limit < 0) {
+        throw new AppError("Page limit is 0", HTTP_STATUS.BAD_REQUEST);
+   }
 
    totalPages = Math.ceil(totalCount / limit);
 
