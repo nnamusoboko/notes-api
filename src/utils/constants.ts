876ee821491cd008871831365a19d21c8822a04b
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-20 13:31:15 +0300

    refactor: extract magic numbers to central constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_PAGE_LIMIT = 10;
+export const DEFAULT_PAGE_NUMBER = 1;
+export const MAX_PAGE_LIMIT = 50;
+
+export const HTTP_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
+}
