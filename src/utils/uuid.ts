a5de5dfd741f8ffde5f27f536d458802620542ad
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-25 14:28:28 +0300

    chore: remove log

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -2,7 +2,3 @@ export const isValidUUID = (id: string): boolean => {
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
     return uuidRegex.test(id);
 };
-
-const string1 = /^$/
-
-console.log(string1.test('hello'));
