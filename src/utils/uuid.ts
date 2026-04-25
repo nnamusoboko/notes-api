19e71cd103b8decf4bc71ba17b175fdf4163ab16
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-25 14:21:36 +0300

    feat: add helper to validate uuids

---
+++ src/utils/uuid.ts
@@ -0,0 +1,8 @@
+export const isValidUUID = (id: string): boolean => {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(id);
+};
+
+const string1 = /^$/
+
+console.log(string1.test('hello'));
