105a49159a789aec70416c3f9f5cd9700358e67c
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-04-25 16:16:25 +0300

    feat: implement save, update, remove, restore, getDeleted using database

--- src/repositories/notes-repo-pg.ts
+++ src/repositories/notes-repo-pg.ts
@@ -1,9 +1,17 @@
-import type { Note } from "../types/types.js";
+import type { ISearchResult, Note, UpdateNoteRequest } from "../types/types.js";
 import { pool } from "../config/database.js";
 import { isValidUUID } from "../utils/uuid.js";
-
+import { AppError } from "../utils/error.js";
+import { HTTP_STATUS } from "../utils/constants.js";
 
 class NotesRepo {
+
+  saveNote = async (note: Note): Promise<Note> => {
+    const query = `INSERT INTO notes (title, contents) VALUES ($1, $2) RETURNING *`;
+    const result = await pool.query(query, [note.title, note.contents]);
+
+    return result.rows[0];
+  }
 
   retrieveNotes = async (offset: number, limit: number): Promise<Note[]> => {
     const query = `SELECT * FROM notes WHERE deleted_at IS NULL ORDER BY created_at DESC LIMIT $1 OFFSET $2`;
@@ -37,6 +45,70 @@ class NotesRepo {
     const result = await pool.query(query);
 
     return Number.parseInt(result.rows[0].count);
+  }
+
+  updateNote = async (noteId: string, noteInfo: UpdateNoteRequest): Promise<Note | null> => {
+    if (!isValidUUID(noteId)) {
+      throw new AppError('Invalid note-id', HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const query = `UPDATE notes SET title = $1, contents = $2 WHERE id = $3 RETURNING *`;
+    const result = await pool.query(query, [noteInfo.title, noteInfo.contents, noteId]);
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    return result.rows[0];
+  }
+
+  removeNote = async (noteId: string): Promise<boolean> => {
+    if (!isValidUUID(noteId)) {
+      throw new AppError('Invalid note-id', HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const query = `UPDATE notes SET deleted_at = NOW(), updated_at = NOW() WHERE (id = $1 RETUNRING AND deleted_at IS NULL)*`;
+    const result = await pool.query(query, [noteId]);
+
+    return result.rows.length === 0;
+  }
+
+  searchByKeyword = async (offset: number, limit: number, search: string): Promise<ISearchResult> => {
+    const searchPattern = `%${search}%`;
+
+    const query = `SELECT * FROM notes WHERE (title ILIKE $1 AND deleted_at IS NULL) LIMIT $1 OFFSET $2`;
+
+    const pageResult = await pool.query(query, [searchPattern, limit, offset]);
+    const notes = pageResult.rows;
+
+    const activeNoteCount = await this.getActiveNoteCount();
+
+    return {
+      matchingList: notes,
+      searchCount: activeNoteCount
+    }   
+  }
+  
+  extractDeletedNotes = async (): Promise<Note[]> => {
+    const query = `SELECT * FROM notes WHERE deleted_at IS NOT NULL`;
+    const results = await pool.query(query);
+
+    return results.rows;
+  }
+  
+  restoreNote = async (noteId: string): Promise<Note | null> => {
+    if (!isValidUUID(noteId)) {
+      throw new AppError('Invalid note-id', HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const query = `SELECT * FROM notes WHERE id = $1 AND deleted_at IS NULL`;
+    const results = await pool.query(query, [noteId]);
+
+    if (results.rows.length == 0) {
+      return null;
+    }
+
+    return results.rows[0];
   }
 
 }
